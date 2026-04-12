ffa6d6e298881b421c09f024c74080c5cb058512
Shaima12 <ichaoui.shaima@gmail.com>
2026-04-13 00:19:34 +0100

    chat v 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ParentDashboard from './components/parent/ParentDashboard'
 import TeacherDashboard from './components/teacher/TeacherDashboard'
-
+import StudentDashboard from './components/student/StudentDashboard'
+import BuddyPage from './components/student/BuddyPage'
 
 function App() {
   return (
-    <div style={{ background: '#F5F4EF', minHeight: '100vh' }}>
-      <ParentDashboard />
-    </div>
+    <BrowserRouter>
+      <div style={{ background: '#F5F4EF', minHeight: '100vh' }}>
+        <Routes>
+          <Route path="/"         element={<ParentDashboard />} />
+          <Route path="/teacher"  element={<TeacherDashboard />} />
+          <Route path="/student"  element={<StudentDashboard />} />
+          <Route path="/buddy"    element={<BuddyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
